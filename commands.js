4291422007d253d9afc52ac9ae51ab63c956c76b
Darya Zakhar <daryazakhar.89@gmail.com>
2023-04-18 11:55:14 +0400

    feat: add commands to the file manager

--- commands.js
+++ commands.js
@@ -1,19 +1,31 @@
-import up from './actions/up.js';
-import addFile from './actions/addFile.js';
-import renameFile from './actions/renameFile.js';
-import readFile from './actions/readFile.js';
-import copyFile from './actions/copyFile.js';
-import moveFile from './actions/moveFile.js';
-import removeFile from './actions/removeFile.js';
+import up from './actions/fs/up.js';
+import addFile from './actions/fs/addFile.js';
+import renameFile from './actions/fs/renameFile.js';
+import readFile from './actions/fs/readFile.js';
+import copyFile from './actions/fs/copyFile.js';
+import moveFile from './actions/fs/moveFile.js';
+import removeFile from './actions/fs/removeFile.js';
+import osModule from './actions/os/index.js';
+import hash from './actions/hash/hash.js';
+import compress from './actions/compessAndDecompress/compress.js';
+import decompress from './actions/compessAndDecompress/decompress.js';
+import changeDirectory from './actions/fs/changeDirectory.js';
+import listFiles from './actions/fs/listFiles.js';
 
 
 export default new Map([
   ['.exit', process.exit],
   ['up', up],
+  ['cd', changeDirectory],
+  ['ls', listFiles],
+  ['cat', readFile],
   ['add', addFile],
   ['rn', renameFile],
-  ['cat', readFile],
   ['cp', copyFile],
   ['mv', moveFile],
   ['rm', removeFile],
+  ['os', osModule],
+  ['hash', hash],
+  ['compress', compress],
+  ['decompress', decompress]
 ]);
